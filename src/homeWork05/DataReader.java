ce7f6b54014a647ee1e0bfbe6d4e500e680065d6
Tumbrigan <ihor.kucher.94@gmail.com>
2020-03-31 15:32:55 +0300

    add Employee and DataReader class

---
+++ src/homeWork05/DataReader.java
@@ -0,0 +1,14 @@
+package homeWork05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DataReader {
+    public static int readInt() throws IOException {
+        System.out.print("Enter you Integer: ");
+        return Integer.parseInt(
+                new BufferedReader(new InputStreamReader(System.in)).readLine());
+    }
+
+}
