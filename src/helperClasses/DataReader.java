d3d2212b44159e48aeb74d2c17e308d0d79ea020
Tumbrigan <ihor.kucher.94@gmail.com>
2020-04-01 11:08:43 +0300

    add enum Months and Task01 class

--- src/helperClasses/DataReader.java
+++ src/helperClasses/DataReader.java
@@ -11,4 +11,14 @@ public class DataReader {
                 new BufferedReader(new InputStreamReader(System.in)).readLine());
     }
 
+    public static int readInt(String offer) throws IOException {
+        System.out.print(offer);
+        return Integer.parseInt(
+                new BufferedReader(new InputStreamReader(System.in)).readLine());
+    }
+
+    public static String readString() throws IOException {
+        System.out.print("Enter you String: ");
+        return new BufferedReader(new InputStreamReader(System.in)).readLine();
+    }
 }
---
+++ src/homeWork05/Task01.java
@@ -0,0 +1,24 @@
+package homeWork05;
+
+import helperClasses.DataReader;
+
+import java.io.IOException;
+
+/*
+Ask user to enter the number of month.
+Read the value and write the amount of days in this month
+(create array with amount days of each month).
+ */
+
+public class Task01 {
+    public static void main(String[] args) throws IOException {
+        Months[] i = Months.values();
+        int monthNumber = DataReader.readInt("Enter the month number: ");
+        if (monthNumber < 1 | monthNumber > 12 ) {
+            System.out.println("Invalid number");
+        } else {
+            String str = i[monthNumber - 1].name();
+            System.out.println(str);
+        }
+    }
+}
