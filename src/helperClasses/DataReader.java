1be73f3f97c23f88419dc093c9bf55d9d07b4650
Tumbrigan <ihor.kucher.94@gmail.com>
2020-04-01 13:19:05 +0300

    update DataReader class

--- src/helperClasses/DataReader.java
+++ src/helperClasses/DataReader.java
@@ -8,17 +8,15 @@ public class DataReader {
     private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public int readInt() throws IOException {
-        System.out.print("Enter you Integer: ");
         return Integer.parseInt(reader.readLine());
     }
 
-    public int readInt(String offer) throws IOException {
+/*    public int readInt(String offer) throws IOException {
         System.out.print(offer);
         return Integer.parseInt(reader.readLine());
-    }
+    }*/
 
     public String readString() throws IOException {
-        System.out.print("Enter you String: ");
         return reader.readLine();
     }
 
