9510845a118a0e7d28bd1a9aa6555722baca2d69
Ihor Kucher <ihor.kucher.94@gmail.com>
2020-04-16 11:19:20 +0300

    add HomeWork11

---
+++ src/main/java/homeWork11/HelloWorld.java
@@ -0,0 +1,11 @@
+package homeWork11;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+        logger.info("Hello World with Maven");
+    }
+}
